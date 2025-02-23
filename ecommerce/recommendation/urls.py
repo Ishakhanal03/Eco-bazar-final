1d0b840755a03f7c3aef225af47ccef6d772cc4f
Ishakhanal03 <ikhanal2003@gmail.com>
2025-02-23 22:22:28 +0545

    added the remaning part

--- ecommerce/recommendation/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from recommendation.views import recommend_products
-
-urlpatterns = [
-    path("recommendations/<str:user_id>/", recommend_products, name="recommend_products"),
-]
